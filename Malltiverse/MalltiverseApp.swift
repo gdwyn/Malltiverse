c1eda7205f8efe10d0f683e5ce618a10034cc694
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2024-07-12 18:33:22 +0100

    Initial Commit

---
+++ Malltiverse/MalltiverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MalltiverseApp.swift
+//  Malltiverse
+//
+//  Created by Godwin IE on 12/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MalltiverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
